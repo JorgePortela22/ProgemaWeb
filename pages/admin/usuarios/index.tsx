5c71dd23ac488c693701ab4af694bfeec4669a94
jorgeportela22 <jorgeportela22@gmail.com>
2021-11-03 13:00:16 -0500

    Agregar link para ir al home desde la pagina /admin/usuarios

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Link from 'next/link'
 const index = () => {
     return (
         <div>
-            ESTOY PROBANDO 
+             <div> Pagima admin usuarios</div>
+             <Link href = '/' > 
+                <a> Ir al Home </a>
+             </Link>
+
         </div>
     )
 }
